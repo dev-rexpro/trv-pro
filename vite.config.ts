c54f0e3143921a0d22cbf331f7669aaa58f9e970
rexcorp1 <rexcorp.inc@gmail.com>
2026-03-07 19:40:45 +0700

    update-3.6p1

--- vite.config.ts
+++ vite.config.ts
@@ -17,17 +17,21 @@ export default defineConfig(({ mode }) => {
           name: 'TRIV Exchange',
           short_name: 'triv',
           description: 'TRIV Exchange PWA',
-          theme_color: '#000000',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone',
           icons: [
             {
               src: 'icons/icon-192x192.png',
               sizes: '192x192',
               type: 'image/png',
+              purpose: 'any maskable',
             },
             {
               src: 'icons/icon-512x512.png',
               sizes: '512x512',
               type: 'image/png',
+              purpose: 'any maskable',
             },
           ],
         },
