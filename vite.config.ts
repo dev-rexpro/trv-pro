d07fc5e242038e101856e3bc77f75343d7f05325
rexcorp1 <rexcorp.inc@gmail.com>
2026-03-08 21:19:17 +0700

    update-3.7p2

--- vite.config.ts
+++ vite.config.ts
@@ -17,62 +17,62 @@ export default defineConfig(({ mode }) => {
           name: 'TRIV Exchange',
           short_name: 'triv',
           description: 'TRIV Exchange PWA',
-          theme_color: '#ffffff',
-          background_color: '#ffffff',
+          theme_color: '#000000',
+          background_color: '#000000',
           display: 'standalone',
           start_url: '/',
           scope: '/',
           icons: [
             {
-              src: '48x48.png',
+              src: 'icons/48x48.png',
               sizes: '48x48',
               type: 'image/png',
               purpose: 'any',
             },
             {
-              src: 'icons/icon-192x192.png',
+              src: 'icons/192x192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'maskable',
             },
             {
-              src: '72x72.png',
+              src: 'icons/72x72.png',
               sizes: '72x72',
               type: 'image/png',
               purpose: 'maskable'
             },
             {
-              src: '96x96.png',
+              src: 'icons/96x96.png',
               sizes: '96x96',
               type: 'image/png',
               purpose: 'maskable'
             },
             {
-              src: '128x128.png',
+              src: 'icons/128x128.png',
               sizes: '128x128',
               type: 'image/png',
               purpose: 'maskable'
             },
             {
-              src: '192x192.png',
+              src: 'icons/192x192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'maskable'
             },
             {
-              src: '384x384.png',
+              src: 'icons/384x384.png',
               sizes: '384x384',
               type: 'image/png',
               purpose: 'maskable'
             },
             {
-              src: '512x512.png',
+              src: 'icons/512x512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any',
             },
             {
-              src: 'icons/icon-512x512.png',
+              src: 'icons/512x512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'maskable',
