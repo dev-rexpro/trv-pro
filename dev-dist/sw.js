1d36700955edef97dbab8b64957eb91a542404c1
rexcorp1 <rexcorp.inc@gmail.com>
2026-03-09 09:58:16 +0700

    update-3.7p3

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9omhg4qj6p8"
+    "revision": "0.0kpjtgsiccg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
